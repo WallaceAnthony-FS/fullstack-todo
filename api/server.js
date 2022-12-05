6edaa33c01ae6d6645511ff60796ecd18ffd0f40
Anthony Wallace <atwallace@student.fullsail.edu>
2022-12-05 07:48:25 -0800

    updated path for default route

--- api/server.js
+++ api/server.js
@@ -24,7 +24,7 @@ app.use("/api/v1/todos", todoRouter)
 // Serve static bundle if route is not /api/*
 app.use(express.static(path.join(__dirname, '../reactjs/build')))
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../reactjs/build', 'index.html'))
+  res.sendFile(path.join(__dirname, '../ui/build', 'index.html'))
 })
 
 
